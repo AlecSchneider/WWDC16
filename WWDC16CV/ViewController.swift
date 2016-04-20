15893dc457e25f341110ac491ed33f48cf1ae593
alec.schneider <alec.schneider@student.hpi.de>
2016-04-20 13:15:10 +0200

    Initial Commit

---
+++ WWDC16CV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WWDC16CV
+//
+//  Created by Alec Schneider on 20/04/16.
+//  Copyright © 2016 Alec Schneider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
